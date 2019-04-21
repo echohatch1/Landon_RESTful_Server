0658a529b27a9dbc8efc7df0f45792557dc31be7
Landon Hatch <landonhatch@gmail.com>
2019-04-20 19:24:09 -0600

    Changed headers

--- index.js
+++ index.js
@@ -28,13 +28,12 @@ app.get('/', (req, res) => {
     res.status(404).send({url: req.originalUrl + ' not found'})
   });*/
 
-//Enable CORS for all HTTP methods
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 
 app.listen(port);
 
