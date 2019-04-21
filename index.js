e23f12d6caa0d47e50d838edb61050292b9d44cf
Landon Hatch <landonhatch@gmail.com>
2019-04-20 20:42:07 -0600

    Changed headers

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require('express'),
   app = express(),
-  port = process.env.PORT || 3000,
+  port = process.env.PORTTWO || 3000,
   mongoose = require('mongoose'),
   Product = require('./api/models/products.model'), //created Product model loading here
   bodyParser = require('body-parser');
 
-  
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});  
+
 // mongoose instance connection url connection
 mongoose.Promise = global.Promise;
 
@@ -20,6 +25,7 @@ app.use(bodyParser.json());
 const productRoute = require('./api/routes/products.routes'); //importing route
 productRoute(app); //register the route
 
+
 app.get('/', (req, res) => {
   res.send("<h1>Welcome to my products API</h1>")
 });
@@ -28,11 +34,6 @@ app.get('/', (req, res) => {
     res.status(404).send({url: req.originalUrl + ' not found'})
   });*/
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
 
 
 app.listen(port);
