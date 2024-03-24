4170553bd6db686cf85662de4579f23631444079
DentalCMODeveloper <77356632+DentalCMODeveloper@users.noreply.github.com>
2024-03-24 09:02:03 -0600

    Removed Netlify specific configs

--- index.js
+++ index.js
@@ -1,5 +1,41 @@
-const functions = require('./functions'); // Import the exported app from functions.js
+const express = require('express'),
+  app = express(),
+  port = process.env.PORT || 3000,
+  mongodbPassword = process.env.MONGO_PASSWORD;
+  mongoose = require('mongoose'),
+  Product = require('./api/models/products.model'), //created Product model loading here
+  bodyParser = require('body-parser');
 
-exports.handler = async (event, context) => {
-  return functions.app(event, context);
-};
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+//Enable CORS for all HTTP methods
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
+// mongoose instance connection url connection
+mongoose.Promise = global.Promise;
+
+mongoose.connect('mongodb+srv://echohatch1:${mongodbPassword}@cluster0.lgu6nng.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', 
+{ useNewUrlParser: true });
+
+
+const productRoute = require('./api/routes/products.routes'); //importing route
+productRoute(app); //register the route
+
+
+app.get('/', (req, res) => {
+  res.send("<h1>Welcome to my products API</h1>")
+});
+
+
+
+app.listen(port);
+
+
+console.log('Products RESTful API server started on: ' + port);
+
